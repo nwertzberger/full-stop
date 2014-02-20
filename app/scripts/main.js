b3911b39dc91aa5ed715c2eba8f4355cc9efa22d
Nicholas Wertzberger <wertnick@gmail.com>
2014-02-20 16:22:20 -0600

    Added game logic.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,37 +6,92 @@ var game = (function(Phaser) {
     var self = this;
 
     self.preload = function() {
-        self.game.load.tilemap('testmap', 'maps/testmap.json', null, Phaser.Tilemap.TILED_JSON);
-        self.game.load.image('village', 'images/village.png');
-        self.game.load.spritesheet('chocobo', 'images/chocobo.png', 24, 32);
+        self.game.load.image('rail', 'images/rail.png');
+        self.game.load.image('car', 'images/car.png');
     };
 
     self.create = function() {
-        self.game.stage.backgroundColor= "#000000";
-        self.map = game.add.tilemap("testmap");
-        self.map.addTilesetImage("village", "village");
-        self.ground = map.createLayer("ground");
-        self.objects = map.createLayer("objects");
-        self.ground.resizeWorld();
+        self.game.stage.backgroundColor= "#444444";
+        self.obstacles = self.game.add.group();
 
-        self.chocobo = game.add.sprite(100,100, 'chocobo');
-        chocobo.animations.add('walk-up',[12,13,14]);
-        chocobo.animations.play('walk-up', 6, true);
+        self.rail = self.obstacles.create(0,0, 'rail');
+        self.rail.body.maxVelocity.y = 200;
+        self.rail.body.minVelocity.y = 0;
 
-        self.cursors = game.input.keyboard.createCursorKeys();
+        self.car = self.game.add.sprite(self.game.world.width/2 - 150,300, 'car');
+
+        self.badFont ={ font: "32px Arial", fill: "#F00" };
+        self.goodFont ={ font: "32px Arial", fill: "#0F0" };
+        self.scoreText = self.game.add.text (16, 16, "Safe Crossings: 0", { font: "32px Arial", fill: "#FFF" });
+        self.lookLeftText = self.game.add.text (16, self.game.world.height - 100, "Look Left!", self.badFont);
+        self.stopText = self.game.add.text (self.game.world.width * .5 - 40, 100,"Stop!", self.badFont);
+        self.lookRightText = self.game.add.text (self.game.world.width - 170, self.game.world.height - 100,  "Look Right!", self.badFont);
+        self.safeCrossings = 0;
+        self.safeStop = false;
+        self.lookedLeft = false;
+        self.lookedRight = false;
+        self.gameOver = false;
+
+        self.cursors = self.game.input.keyboard.createCursorKeys();
     };
 
     self.update = function() {
+        if (self.gameOver) {
+            return;
+        }
 
+        if (self.cursors.up.isDown) {
+            self.rail.body.acceleration.y = 200;
+        } else if (self.cursors.down.isDown) {
+            self.rail.body.acceleration.y = -300;
+        }
+
+        if (self.cursors.left.isDown) {
+            self.lookedLeft = true;
+            self.lookLeftText.font = self.goodFont;
+        } else if (self.cursors.right.isDown) {
+            self.lookedRight = true;
+            self.lookRightText.font = self.goodFont;
+        }
+
+        if (self.rail.body.y > self.game.world.height) {
+            self.rail.body.y = 0;
+            self.passedRail = false;
+        }
+        if (self.rail.body.velocity.y < 0) {
+            self.rail.body.velocity.y = 0;
+            self.rail.body.acceleration.y = 0;
+            self.safeStop = true;
+            self.stopText.font = self.goodFont;
+        }
+
+        if (self.rail.body.y > self.car.body.y && !self.passedRail) {
+            self.passedRail = true;
+            if (self.safeStop && self.lookedLeft && self.lookedRight) {
+                self.safeCrossings++;
+                self.scoreText.content = "Safe Crossings: " + self.safeCrossings;
+            } else {
+                self.gameOver = true;
+                self.game.add.text (130, self.game.world.height * 0.4, "Game Over!", { font: "64px Arial", fill: "#F00" });
+            }
+
+            self.safeStop = false;
+            self.lookedLeft = false;
+            self.lookedRight = false;
+            
+            self.stopText.font = self.badFont;
+            self.lookLeftText.font = self.badFont;
+            self.lookRightText.font = self.badFont;
+        }
     };
 
     self.render = function() {
-        self.game.debug.renderCameraInfo(game.camera, 16, 16);
+        self.game.debug.renderCameraInfo(self.game.camera, 16, 16);
     };
 
     self.go = function() {
         self.game = new Phaser.Game(
-                320, 480,
+                640, 480,
                 Phaser.AUTO,
                 'game-window', 
                 self);
